988ca378508bc4ad09497b6fddf79abf238f4861
alexander <alexander_199@hotmail.se>
2025-03-17 14:12:46 +0100

    first commit

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,18 @@
+"use client"
+import Authform from '@/components/Authform'
+import { signInWithCredentials } from '@/lib/actions/auth'
+import { signInSchema } from '@/lib/validations'
+import React from 'react'
+
+const page = () => {
+  return (
+    <Authform
+       type='SIGN_IN'
+       schema={signInSchema}
+       defaultValues={{email: '', password: ''}}
+       onSubmit={signInWithCredentials}
+    />
+  )
+}
+
+export default page
---
+++ components/BookList.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import BookCard from './BookCard';
+
+
+interface Props {
+    title: string;
+    books: Book[];
+    containerclassName?: string;
+}
+
+const BookList = ({title , books, containerclassName }: Props) => {
+  return (
+    <section className={`container ${containerclassName}`}>
+        <h2 className='font-bebas-neue text-4xl text-light-100'>
+            {title}
+        </h2>
+        <ul className='book-list'>
+            {books.map((book) => (
+              <BookCard key={book.title} {...book}/>
+            ))}
+        </ul>
+    </section>
+  )
+}
+
+export default BookList
